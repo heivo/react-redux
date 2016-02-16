5322caef3d48c14826a15cd47b1cbc444b5d077d
ivo <ivo@heidelba.ch>
2016-02-17 00:26:17 +0100

    replace logging reducer with middleware

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, hashHistory  } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
 
 import Main from './components/Main'
@@ -10,16 +10,13 @@ import Home from './components/Home'
 import Content1 from './components/Content1'
 import Content2 from './components/Content2'
 
-const loggingReducer = (state = {}, action) => {
-	console.log(action);
-	return state;
-};
+import logger from './middleware/logger'
 
-const reducers = combineReducers(Object.assign({}, { loggingReducer }, {
+const reducers = combineReducers(Object.assign({}, {
 	routing: routerReducer
 }));
 
-const store = createStore(reducers, applyMiddleware(routerMiddleware(hashHistory)));
+const store = createStore(reducers, applyMiddleware(logger, routerMiddleware(hashHistory)));
 // syncHistoryWithStore(hashHistory, store);
 
 const App = () => (
